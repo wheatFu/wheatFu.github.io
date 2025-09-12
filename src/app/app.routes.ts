359ac7f90ae312339d8e29a9a10cc5f42ecac1f6
wheat <wheat.fu@globalpay.com>
2025-09-12 15:26:04 +0800

    feat: test

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { Operator } from '../pages/operator/operator';
-
-export const routes: Routes = [
-	{ path: 'operator/:name', component: Operator },
-	{ path: '', redirectTo: '/operator/defer', pathMatch: 'full' }
-];
--- src/types/highlightjs.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'highlight.js/lib/core' {
-  const hljs: any;
-  export default hljs;
-}
-declare module 'highlight.js/lib/languages/*' {
-  const lang: any;
-  export default lang;
-}
